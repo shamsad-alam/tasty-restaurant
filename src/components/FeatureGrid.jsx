f67b44728be2eb26e87d6b83f5f24fac37e6629e
shamsad-alam <alamshamsad687@gmail.com>
2025-10-26 13:38:28 +0530

    featureGrid componnt some changes

--- src/components/FeatureGrid.jsx
+++ src/components/FeatureGrid.jsx
@@ -9,8 +9,8 @@ function FeatureGrid() {
     <>
       <section className='rounded-4xl bg-red-800 py-12 mx-4'>
         <div className='w-7xl mx-auto px-4  '>
-          <div className='flex md:w-full lg:w-1/2 xl:w-1/4 justify-between'>
-            <div className='text-white'>
+          <div className='flex  w-1/4 gap-12 justify-between'>
+            <div className='text-white flex-1'>
               <span><GiDeliveryDrone className='w-[100px] h-[100px] text-7xl p-4 rounded-2xl bg-green-500' /></span>
               <h3 className='text-2xl font-bold'>Super Fast Order</h3>
             </div>
